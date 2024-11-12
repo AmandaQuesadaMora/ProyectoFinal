f24f9b1d06c3bf3b08af2d998457ed7f31d4bf4a
jason <j.solorzano1910@gmail.com>
2024-11-12 17:20:56 -0600

    AR-03[Registro de usuarios]

--- src/main/java/com/segdyma/controller/UsuarioController.java
+++ src/main/java/com/segdyma/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,5 +40,45 @@ public class UsuarioController {
     @GetMapping("/menu")
     public String mostrarMenu() {
         return "menu"; // Página del menú principal después de iniciar sesión
+    }
+    
+    
+   
+    // Mostrar el formulario de registro
+    @GetMapping("/registro")
+    public String mostrarFormularioRegistro(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String registrarUsuario(@ModelAttribute Usuario usuario, Model model) {
+        // Validar si el usuario ya existe
+        Usuario existente = usuarioService.findByUsername(usuario.getUsername());
+
+        if (existente != null) {
+            model.addAttribute("error", "El nombre de usuario ya existe.");
+            return "registro";
+        }
+
+        // Validar la contraseña
+        String password = usuario.getPassword();
+        if (password == null || password.isEmpty()) {
+            model.addAttribute("error", "La contraseña no puede estar vacía.");
+            return "registro";
+        }
+
+        String passwordPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*(),.?\":{}|<>]).{6,}$";
+        if (!password.matches(passwordPattern)) {
+            model.addAttribute("error", "La contraseña debe tener al menos una letra mayúscula, una letra minúscula, un carácter especial y tener mínimo 6 caracteres.");
+            return "registro";
+        }
+
+        // Guardar el nuevo usuario en la base de datos
+        usuario.setActivo(true);
+        usuarioService.save(usuario);
+
+        model.addAttribute("success", "Usuario registrado exitosamente.");
+        return "registro";
     }
 }
